918ae930d54a8190edbb3146ab80e2cc1aba9008
Lans12345 <olanalans12345@gmail.com>
2022-10-11 07:00:17 +0800

    added custom widgets

---
+++ lib/views/widgets/textformfield_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sample_app/views/widgets/text_widget.dart';
+
+class TextFormFieldWidget extends StatelessWidget {
+  late TextEditingController inputController;
+
+  TextFormFieldWidget({required this.inputController});
+
+  void onChanged(String) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: inputController,
+      decoration: InputDecoration(
+        label: TextWidget(
+            text: 'Input',
+            color: Colors.black,
+            fontSize: 14,
+            fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
